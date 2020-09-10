869c56b96e3fa20e07b3e772422483ae2e877518
Ryan Mehta <ryan.mehta@gmail.com>
2020-09-10 15:52:51 -0700

    create server

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true, use
   .then(client => {
     const evacuations = client.db('fire-gis').collection('evacuations');
     const server = http.createServer((req, res) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET OPTIONS');
       const index = req.url.split('/').slice(-1);
       evacuations.findOne({}, { limit: 1, skip: +index })
         .then(evacuation => res.end(JSON.stringify(evacuation)));
