01b2a1213ec0a7fb7a1d996502266ffe5e64db6c
Ryan Mehta <ryan.mehta@gmail.com>
2020-09-10 15:39:32 -0700

    create server

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const { MongoClient } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(client => {
+    const evacuations = client.db('fire-gis').collection('evacuations');
+    const server = http.createServer((req, res) => {
+      const index = req.url.split('/').slice(-1);
+      evacuations.findOne({}, { limit: 1, skip: +index })
+        .then(evacuation => res.end(JSON.stringify(evacuation)));
+    });
+    
+    server.listen(9191);
+  });
